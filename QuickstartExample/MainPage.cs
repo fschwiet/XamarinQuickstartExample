a25afaf18f8f87e3beba8e783985ac88cd764e49
fschwiet <fschwiet@gmail.com>
2016-08-21 13:29:24 -0700

    first commit (following tutorial at https://developer.xamarin.com/guides/xamarin-forms/getting-started/hello-xamarin-forms/ but without using XAML)

---
+++ QuickstartExample/MainPage.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace QuickstartExample
+{
+	public class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			Padding = new Thickness(20);
+			Device.OnPlatform(() => this.Padding = new Thickness(20, 40, 20, 20));
+
+			var phoneNumberTextEntry = new Entry
+			{
+				Text = "1-855-XAMARIN"
+			};
+
+			var translateButton = new Button { Text = "Translate" };
+			translateButton.Clicked += (sender, e) => { };
+
+			var callButton = new Button { Text = "Call", IsEnabled = false };
+			callButton.Clicked += (sender, e) => { };
+
+			Content = new StackLayout
+			{
+				VerticalOptions = new LayoutOptions(LayoutAlignment.Fill, true),
+				HorizontalOptions = new LayoutOptions(LayoutAlignment.Fill, true),
+				Orientation = StackOrientation.Vertical,
+				Spacing = 15,
+				Children = {
+					new Label { Text = "Enter a phoneword:" },
+					phoneNumberTextEntry,
+					translateButton,
+					callButton
+
+				}
+			};
+		}
+	}
+}
+
+
---
+++ QuickstartExample/MyPage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace QuickstartExample
+{
+	public class MyPage : ContentPage
+	{
+		public MyPage()
+		{
+			Content = new StackLayout
+			{
+				Children = {
+					new Label { Text = "Hello ContentPage" }
+				}
+			};
+		}
+	}
+}
+
+
